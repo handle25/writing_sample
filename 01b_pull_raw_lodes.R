4f6d87d60c1924ea29ae6cd8c4f8d4f41b9f0430
sohandle <handleysoph@gmail.com>
2026-08-08 11:44:28 -0400

    raw lodes data pull

--- 01b_pull_raw_lodes.R
+++ 01b_pull_raw_lodes.R
@@ -12,19 +12,20 @@ library(stringr)
 library(jsonlite)
 library(httr)
 library(lehdr)
+library(tidygeocoder)
 
 
 path <- "C:/Users/Sophie/Desktop/phd_apps/writing_sample/data/lodes"
 
 states <- tolower(state.abb) 
+states <- c()
 years <- seq(2002, 2020)
 
 available <- list()
-
 for (s in states) {
   for (y in years) {
     
-    x <- tryCatch(
+    lodes <- tryCatch(
       grab_lodes(
         state = s,
         year = y,
@@ -34,11 +35,40 @@ for (s in states) {
       error = function(e) NULL
     )
     
-    if (!is.null(x)) {
-      available[[length(available) + 1]] <- x
-      print(paste("Success:", s, y))
-    } else {
-      print(paste("Missing:", s, y))
+    if (!is.null(lodes)) {
+      
+      lodes <- data.table(lodes)
+      lodes[, `:=`(state = s, year = y)]
+      
+      fwrite(
+        lodes,
+        paste0(path, "/lodes_", s, "_", y, ".csv")
+      )
+      
+      rm(lodes)
+      gc()
+      
+      print(paste("Saved:", s, y))
     }
   }
 }
+
+
+mi_files <- list.files(path, pattern = "^lodes_mi_.*\\.csv$", full.names = TRUE)
+
+file_list <- list()
+
+for (i in seq_along(mi_files)) {
+  
+  dt <- fread(mi_files[i])
+  
+  file_list[[i]] <- dt
+  
+}
+
+mi <- rbindlist(file_list, fill = TRUE)
+# save
+fwrite(
+  mi,
+  paste0(path, "/clean/mi_full_2002_2020.csv")
+)
