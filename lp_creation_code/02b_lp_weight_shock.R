0969de45fae2f0c30b6a1a062fedc4a9c0933154
sohandle <handleysoph@gmail.com>
2026-08-15 20:33:25 -0400

    adding QCEW data for LPs, annual values all scaled by 1995 pre period values

---
+++ lp_creation_code/02b_lp_weight_shock.R
@@ -0,0 +1,204 @@
+################################################################################
+# Created 8.8.2026 
+# Author: Sophie Handley 
+# Purpose: Weight dollar shifts in naics x year imports by labor shares with QCEW
+
+################################################################################
+
+rm(list = ls())
+
+# qcewdata 
+path <- "D:/writing_sample/data"
+setwd(path)
+
+# read in country industry level data ------------------------------------------
+# has location area_fips which is county level 
+# save
+qcew_naics3 <- fread(paste0(path, "/qcew/clean/new_full_qcew_1995_2025.csv"))
+qcew_naics3[, industry_code := as.integer(industry_code)]
+qcew_naics3[,area_fips := as.character(as.integer(area_fips))]
+
+# trade data -> naics for naics level shock ------------------------------------ 
+shock <- fread(paste0(path, "/output/Delta_M_naics3_lp.csv"))
+
+# population weights -----------------------------------------------------------
+acs <- rbind(fread(paste0(path, "/acs/co-est00int-tot.csv")), 
+             fread(paste0(path,"/acs/co-est2020.csv")), fill = TRUE)
+
+acs <- melt(
+  acs,
+  id.vars = c("STATE", "COUNTY"),
+  measure.vars = patterns("^POPESTIMATE"),
+  variable.name = "year",
+  value.name = "population"
+) |>
+  fmutate(year = as.integer(substr(year, 12, 15)))
+
+# Drop rows created from years not covered by that source file
+acs <- acs[!is.na(population)]
+
+# Drop state totals
+acs <- acs[COUNTY != 0]
+
+acs[, area_fips := as.character(paste0(
+  sprintf("%02d", STATE),
+  sprintf("%03d", COUNTY)
+))]
+
+pop_list <- list()
+for (y in c(1995:1999)){
+  url <- paste0(
+    "https://www2.census.gov/programs-surveys/popest/",
+    "tables/1990-2000/intercensal/st-co/stch-icen", y, ".txt"
+  )
+  
+  pop <- fread(url)
+  
+  names <- c("year","area_fips","age","sex","eth","population")
+  
+  names(pop) <- names
+  pop <- pop |> 
+    fgroup_by(area_fips) |> 
+    fsummarize(population = fsum(population)) |> 
+    fmutate(year = y) 
+  
+  pop_list[[length(pop_list) + 1]] <- pop
+}
+
+pop <- rbindlist(pop_list) 
+acs <- rbind(acs, pop, fill = TRUE)
+acs[,area_fips := as.character(as.integer(area_fips))]
+
+# Create QCEW employment weights -----------------------------------------------
+# take one base period for now 
+qcew_base <- qcew_naics3[year == 1995,]
+qcew_base <- merge(qcew_base, acs, 
+                   by = c("area_fips", "year"), 
+                   all.x = TRUE)
+
+# Baseline county employment
+qcew_base[, L_it := sum(annual_avg_emplvl, na.rm = TRUE),
+         by = .(area_fips)]
+
+# Baseline county-industry employment
+qcew_base[, L_ijt := annual_avg_emplvl]
+
+# Baseline US employment in industry j
+qcew_base[, L_ujt := sum(annual_avg_emplvl, na.rm = TRUE),
+         by = .(industry_code)]
+
+qcew_base <- qcew_base |> fselect(area_fips, 
+                                  industry_code, 
+                                  L_it, L_ijt, L_ujt, 
+                                  population)
+
+nrow(qcew_naics3)
+qcew_naics3 <- merge(
+  qcew_naics3, qcew_base, 
+  by = c("area_fips", "industry_code")
+)
+nrow(qcew_naics3)
+
+# Merge both trade shocks onto the same baseline employment data
+qcew_rep <- merge(
+  qcew_naics3,
+  shock,
+  by.x = c("year", "industry_code"),
+  by.y = c("refYear", "naics3"),
+  all.x = TRUE
+)
+
+# Industries with no trade match contribute zero
+qcew_rep[is.na(Delta_M_US),  Delta_M_US := 0]
+qcew_rep[is.na(Delta_M_OTH), Delta_M_OTH := 0]
+
+# Same employment weights applied to both trade changes
+qcew_rep[, IPW_US :=
+           (L_ijt / L_ujt) * (Delta_M_US / population)]
+
+qcew_rep[, IPW_OTH :=
+           (L_ijt / L_ujt) * (Delta_M_OTH / population)]
+
+# scale by 1000 for thousand dollars per worker hour 
+qcew_rep[, IPW_US  := IPW_US / 1000]
+qcew_rep[, IPW_OTH := IPW_OTH / 1000]
+
+
+# Collapse across industries to county x period
+instrument <- qcew_rep |>
+  fgroup_by(area_fips, year) |>
+  fsummarize(
+    IPW_US  = fsum(IPW_US),
+    IPW_OTH = fsum(IPW_OTH)
+  ) |>
+  data.table()
+fwrite(instrument, file = paste0(path, "/output/lp_final_ipw_naics3.csv"))
+
+# Get employment outcome -------------------------------------------------------
+# Use NAICS2 data since manufacturing is identified cleanly there
+qcew_outcome <- rbindlist(qcew_naics3)
+qcew_outcome[, industry_code := as.integer(industry_code)]
+qcew_outcome[,naics2:= floor(as.integer(industry_code/10))]
+
+
+# Total county employment
+county_emp <- qcew_outcome |>
+  fgroup_by(area_fips, year) |>
+  fsummarize(
+    total_emp = fsum(annual_avg_emplvl)
+  ) |>
+  data.table()
+
+# Manufacturing county employment
+county_manufac <- qcew_outcome[
+  naics2 %in% c(31, 32, 33)
+] |>
+  fgroup_by(area_fips, year) |>
+  fsummarize(
+    manufac_emp = fsum(annual_avg_emplvl)
+  ) |>
+  data.table()
+
+# Merge and construct manufacturing employment share
+county_emp <- merge(
+  county_emp,
+  county_manufac,
+  by = c("area_fips", "year"),
+  all.x = TRUE
+)
+
+county_emp[is.na(manufac_emp), manufac_emp := 0]
+
+county_emp[, sh_empl_mfg := manufac_emp / total_emp]
+
+
+# Long differences: 1995-2000 and 2000-2007
+setorder(county_emp, area_fips, year)
+
+county_emp[, d_sh_empl_mfg :=
+             sh_empl_mfg - shift(sh_empl_mfg),
+           by = area_fips
+]
+# baseline manufacturing share control
+county_emp[, l_shind_manuf :=
+             shift(sh_empl_mfg),
+           by = area_fips
+]
+
+# baseline employment weight as a temporary county analogue
+county_emp[, baseline_emp :=
+             shift(total_emp),
+           by = area_fips
+]
+
+# Regressions ------------------------------------------------------------------
+# Merge onto the two-period instrument
+
+base <- merge(
+  county_emp,
+  instrument,
+  by = c("area_fips", "year")
+)
+
+# save file 
+fwrite(base, paste0(path, "/output/lp_weighted_qcew.csv"))
---
+++ lp_creation_code/03_lp_merge_transform.R
@@ -0,0 +1,742 @@
+################################################################################
+# Created 8.15.2026 
+# Author: Sophie Handley 
+# Purpose: Construct regression outcomes and merge QCEW, LODES, IRS, population
+################################################################################
+
+
+rm(list = ls())
+
+# qcewdata 
+path <- "D:/writing_sample/data"
+setwd(path)
+
+################################################################################
+# Population
+################################################################################
+
+acs <- rbind(
+  fread(paste0(path, "/acs/co-est00int-tot.csv")), 
+  fread(paste0(path, "/acs/co-est2020.csv")),
+  fill = TRUE
+)
+
+acs <- melt(
+  acs,
+  id.vars = c("STATE", "COUNTY"),
+  measure.vars = patterns("^POPESTIMATE"),
+  variable.name = "year",
+  value.name = "population"
+) |>
+  fmutate(year = as.integer(substr(year, 12, 15)))
+
+# Drop rows created from years not covered by that source file
+acs <- acs[!is.na(population)]
+
+# Drop state totals
+acs <- acs[COUNTY != 0]
+
+# Create county FIPS
+acs[, area_fips := as.integer(paste0(
+  sprintf("%02d", STATE),
+  sprintf("%03d", COUNTY)
+))]
+acs[,area_fips := as.character(as.integer(area_fips))]
+
+# population -------------------------------------------------------------------
+
+pop_list <- list()
+for (y in c(1995:1999)){
+  url <- paste0(
+    "https://www2.census.gov/programs-surveys/popest/",
+    "tables/1990-2000/intercensal/st-co/stch-icen", y, ".txt"
+  )
+  
+  pop <- fread(url)
+  
+  names <- c("year","area_fips","age","sex","eth","population")
+  
+  names(pop) <- names
+  pop <- pop |> 
+    fgroup_by(area_fips) |> 
+    fsummarize(population = fsum(population)) |> 
+    fmutate(year = y) 
+  
+  pop_list[[length(pop_list) + 1]] <- pop
+}
+
+pop <- rbindlist(pop_list) 
+acs <- rbind(acs, pop, fill = TRUE)
+
+################################################################################
+# Read datasets
+################################################################################
+
+qcew <- fread(
+  paste0(path, "/output/lp_weighted_qcew.csv")
+)
+
+qcew[, .(
+  N = .N,
+  mean_US   = mean(IPW_US, na.rm = TRUE),
+  median_US = median(IPW_US, na.rm = TRUE),
+  min_US    = min(IPW_US, na.rm = TRUE),
+  max_US    = max(IPW_US, na.rm = TRUE),
+  mean_OTH  = mean(IPW_OTH, na.rm = TRUE)
+), by = year]
+
+qcew[, area_fips_str := sprintf("%06d", area_fips)]
+qcew[, state := floor(area_fips / 1000)]
+
+# PAUSE INCOMPLETE 
+# LODES ------------------------------------------------------------------------
+
+lodes <- fread(
+  paste0(
+    path,
+    "/output/lodes_collapsed_all_no_crosswalk.csv"
+  )
+)
+
+lodes[
+  year %in% c(2002, 2003, 2004),
+  year := 2000
+]
+
+
+# IRS --------------------------------------------------------------------------
+
+irs <- fread(
+  paste0(path, "/irs_migration_full.csv")
+)
+
+
+################################################################################
+# Merge datasets
+################################################################################
+
+reg <- merge(
+  qcew,
+  lodes,
+  by.x = c("area_fips", "year"),
+  by.y = c("county", "year")
+)
+
+nrow(reg)
+
+reg <- merge(
+  reg,
+  irs,
+  by = c("area_fips", "year"),
+  all.x = TRUE
+)
+
+nrow(reg)
+
+reg <- merge(
+  reg,
+  acs,
+  by = c("area_fips", "year"),
+  all.x = TRUE
+)
+
+setorder(
+  reg,
+  area_fips,
+  year
+)
+
+
+################################################################################
+# Rename fundamental employment concepts
+################################################################################
+
+# QCEW:
+# Employment located at establishments in the county
+setnames(
+  reg,
+  "total_emp",
+  "workplace_emp"
+)
+
+# LODES:
+# Employed residents of the county, regardless of workplace county
+setnames(
+  reg,
+  "total_jobs",
+  "resident_emp"
+)
+
+
+################################################################################
+# Basic regression variables
+################################################################################
+
+reg[, t2 := as.integer(year == 2013)]
+
+# State FIPS for clustering
+reg[, statefip :=
+      floor(as.integer(area_fips) / 1000)]
+
+
+################################################################################
+# Net migration flows
+################################################################################
+
+reg[, net_migration :=
+      as.integer(returns_3_inflow) -
+      as.integer(returns_3_outflow)]
+
+# Net migration relative to employed residents
+reg[, net_migration_share_resident_emp :=
+      net_migration / resident_emp]
+
+reg[, net_migration_share_workplace_emp :=
+      net_migration / workplace_emp]
+
+reg[, net_migration_share_population :=
+      net_migration / population]
+
+reg[, net_migration_share_population_t0 :=
+      net_migration / shift(population, type = "lag", n = 1), by = .(area_fips)]
+
+reg[, d_net_migration_pctchange :=
+      (net_migration -
+         shift(net_migration, n = 1)) /
+      shift(net_migration, n = 1),
+    by = area_fips]
+
+
+# Winsorize net migration share -----------------------------------------------
+for (v in c("net_migration_share_workplace_emp", 
+            "net_migration_share_resident_emp", 
+            "net_migration_share_population",
+            "net_migration_share_population_t0")){
+  w_var <- paste0("w_", v)
+  
+  q99 <- quantile(
+    reg[, get(v)],
+    .99,
+    na.rm = TRUE
+  )
+  
+  q01 <- quantile(
+    reg[, get(v)],
+    .01,
+    na.rm = TRUE
+  )
+  
+  reg[, (w_var) :=
+        get(v)]
+  
+  reg[
+    get(v) >= q99,
+    (w_var) := q99
+  ]
+  
+  reg[
+    get(v) <= q01,
+    (w_var) := q01
+  ]
+  
+  
+  
+}
+
+
+
+# Winsorize change in net migration -------------------------------------------
+
+q99 <- quantile(
+  reg[, d_net_migration_pctchange],
+  .99,
+  na.rm = TRUE
+)
+
+q01 <- quantile(
+  reg[, d_net_migration_pctchange],
+  .01,
+  na.rm = TRUE
+)
+
+reg[, w_d_net_migration_pctchange :=
+      d_net_migration_pctchange]
+
+reg[
+  d_net_migration_pctchange >= q99,
+  w_d_net_migration_pctchange := q99
+]
+
+reg[
+  d_net_migration_pctchange <= q01,
+  w_d_net_migration_pctchange := q01
+]
+
+
+################################################################################
+# Employment-to-population ratios
+################################################################################
+
+# Employment of county residents / county population
+reg[, resident_emp_population_ratio :=
+      resident_emp / population]
+
+# Employment located in county / county population
+reg[, workplace_emp_population_ratio :=
+      workplace_emp / population]
+
+
+# Differences ------------------------------------------------------------------
+
+reg[, d_resident_emp_population_ratio :=
+      resident_emp_population_ratio -
+      shift(
+        resident_emp_population_ratio,
+        n = 1
+      ),
+    by = area_fips]
+
+reg[, d_workplace_emp_population_ratio :=
+      workplace_emp_population_ratio -
+      shift(
+        workplace_emp_population_ratio,
+        n = 1
+      ),
+    by = area_fips]
+
+
+# Winsorize resident employment / population ----------------------------------
+
+q99 <- quantile(
+  reg$d_resident_emp_population_ratio,
+  .99,
+  na.rm = TRUE
+)
+
+q01 <- quantile(
+  reg$d_resident_emp_population_ratio,
+  .01,
+  na.rm = TRUE
+)
+
+reg[, w_d_resident_emp_population_ratio :=
+      d_resident_emp_population_ratio]
+
+reg[
+  d_resident_emp_population_ratio >= q99,
+  w_d_resident_emp_population_ratio := q99
+]
+
+reg[
+  d_resident_emp_population_ratio <= q01,
+  w_d_resident_emp_population_ratio := q01
+]
+
+
+# Winsorize workplace employment / population ---------------------------------
+
+q99 <- quantile(
+  reg$d_workplace_emp_population_ratio,
+  .99,
+  na.rm = TRUE
+)
+
+q01 <- quantile(
+  reg$d_workplace_emp_population_ratio,
+  .01,
+  na.rm = TRUE
+)
+
+reg[, w_d_workplace_emp_population_ratio :=
+      d_workplace_emp_population_ratio]
+
+reg[
+  d_workplace_emp_population_ratio >= q99,
+  w_d_workplace_emp_population_ratio := q99
+]
+
+reg[
+  d_workplace_emp_population_ratio <= q01,
+  w_d_workplace_emp_population_ratio := q01
+]
+
+
+################################################################################
+# Variables to difference in levels
+################################################################################
+
+level_vars <- c(
+  grep("^outside.*_jobs$", names(reg), value = TRUE),
+  grep("^total.*_jobs$", names(reg), value = TRUE),
+  grep("^inside.*_jobs$", names(reg), value = TRUE)
+)
+
+level_vars <- unique(level_vars)
+
+
+for (i in level_vars) {
+  
+  ##############################################################################
+  # Denominator = resident employment
+  ##############################################################################
+  
+  var_d_all <- paste0(
+    i,
+    "_share_resident_emp"
+  )
+  
+  d_var_d_all <- paste0(
+    "d_",
+    i,
+    "_share_resident_emp"
+  )
+  
+  reg[, (var_d_all) :=
+        get(i) / resident_emp]
+  
+  reg[, (d_var_d_all) :=
+        get(var_d_all) -
+        shift(
+          get(var_d_all),
+          type = "lag",
+          n = 1
+        ),
+      by = area_fips]
+  
+  w_var_d_all <- paste0(
+    "w_",
+    d_var_d_all
+  )
+  
+  q99 <- quantile(
+    reg[[d_var_d_all]],
+    .99,
+    na.rm = TRUE
+  )
+  
+  q01 <- quantile(
+    reg[[d_var_d_all]],
+    .01,
+    na.rm = TRUE
+  )
+  
+  reg[, (w_var_d_all) :=
+        get(d_var_d_all)]
+  
+  reg[
+    get(w_var_d_all) >= q99,
+    (w_var_d_all) := q99
+  ]
+  
+  reg[
+    get(w_var_d_all) <= q01,
+    (w_var_d_all) := q01
+  ]
+  
+  ##############################################################################
+  # Denominator = local workplace employment
+  ##############################################################################
+  
+  var_d_all <- paste0(
+    i,
+    "_share_workplace_emp"
+  )
+  
+  d_var_d_all <- paste0(
+    "d_",
+    i,
+    "_share_workplace_emp"
+  )
+  
+  reg[, (var_d_all) :=
+        get(i) / workplace_emp]
+  
+  reg[, (d_var_d_all) :=
+        get(var_d_all) -
+        shift(
+          get(var_d_all),
+          type = "lag",
+          n = 1
+        ),
+      by = area_fips]
+  
+  w_var_d_all <- paste0(
+    "w_",
+    d_var_d_all
+  )
+  
+  q99 <- quantile(
+    reg[[d_var_d_all]],
+    .99,
+    na.rm = TRUE
+  )
+  
+  q01 <- quantile(
+    reg[[d_var_d_all]],
+    .01,
+    na.rm = TRUE
+  )
+  
+  reg[, (w_var_d_all) :=
+        get(d_var_d_all)]
+  
+  reg[
+    get(w_var_d_all) >= q99,
+    (w_var_d_all) := q99
+  ]
+  
+  reg[
+    get(w_var_d_all) <= q01,
+    (w_var_d_all) := q01
+  ]
+  
+  
+  ##############################################################################
+  # Denominator = outside employment
+  ##############################################################################
+  
+  var_d_out <- paste0(
+    i,
+    "_share_outside_jobs"
+  )
+  
+  d_var_d_out <- paste0(
+    "d_",
+    i,
+    "_share_outside_jobs"
+  )
+  
+  reg[, (var_d_out) :=
+        get(i) / outside_jobs]
+  
+  reg[, (d_var_d_out) :=
+        get(var_d_out) -
+        shift(
+          get(var_d_out),
+          type = "lag",
+          n = 1
+        ),
+      by = area_fips]
+  
+  w_var_d_out <- paste0(
+    "w_",
+    d_var_d_out
+  )
+  
+  q99 <- quantile(
+    reg[[d_var_d_out]],
+    .99,
+    na.rm = TRUE
+  )
+  
+  q01 <- quantile(
+    reg[[d_var_d_out]],
+    .01,
+    na.rm = TRUE
+  )
+  
+  reg[, (w_var_d_out) :=
+        get(d_var_d_out)]
+  
+  reg[
+    get(w_var_d_out) >= q99,
+    (w_var_d_out) := q99
+  ]
+  
+  reg[
+    get(w_var_d_out) <= q01,
+    (w_var_d_out) := q01
+  ]
+  
+  
+  ##############################################################################
+  # Denominator = resident service employment
+  ##############################################################################
+  
+  var_d_svc <- paste0(
+    i,
+    "_share_service_jobs"
+  )
+  
+  d_var_d_svc <- paste0(
+    "d_",
+    i,
+    "_share_service_jobs"
+  )
+  
+  reg[, (var_d_svc) :=
+        get(i) / total_servc_jobs]
+  
+  reg[, (d_var_d_svc) :=
+        get(var_d_svc) -
+        shift(
+          get(var_d_svc),
+          type = "lag",
+          n = 1
+        ),
+      by = area_fips]
+  
+  w_var_d_svc <- paste0(
+    "w_",
+    d_var_d_svc
+  )
+  
+  q99 <- quantile(
+    reg[[d_var_d_svc]],
+    .99,
+    na.rm = TRUE
+  )
+  
+  q01 <- quantile(
+    reg[[d_var_d_svc]],
+    .01,
+    na.rm = TRUE
+  )
+  
+  reg[, (w_var_d_svc) :=
+        get(d_var_d_svc)]
+  
+  reg[
+    get(w_var_d_svc) >= q99,
+    (w_var_d_svc) := q99
+  ]
+  
+  reg[
+    get(w_var_d_svc) <= q01,
+    (w_var_d_svc) := q01
+  ]
+  
+  
+  ##############################################################################
+  # Denominator = resident goods employment
+  ##############################################################################
+  
+  var_d_good <- paste0(
+    i,
+    "_share_goods_jobs"
+  )
+  
+  d_var_d_good <- paste0(
+    "d_",
+    i,
+    "_share_goods_jobs"
+  )
+  
+  reg[, (var_d_good) :=
+        get(i) / total_goods_jobs]
+  
+  reg[, (d_var_d_good) :=
+        get(var_d_good) -
+        shift(
+          get(var_d_good),
+          type = "lag",
+          n = 1
+        ),
+      by = area_fips]
+  
+  w_var_d_good <- paste0(
+    "w_",
+    d_var_d_good
+  )
+  
+  q99 <- quantile(
+    reg[[d_var_d_good]],
+    .99,
+    na.rm = TRUE
+  )
+  
+  q01 <- quantile(
+    reg[[d_var_d_good]],
+    .01,
+    na.rm = TRUE
+  )
+  
+  reg[, (w_var_d_good) :=
+        get(d_var_d_good)]
+  
+  reg[
+    get(w_var_d_good) >= q99,
+    (w_var_d_good) := q99
+  ]
+  
+  reg[
+    get(w_var_d_good) <= q01,
+    (w_var_d_good) := q01
+  ]
+  
+  
+  ##############################################################################
+  # Denominator = population
+  ##############################################################################
+  
+  var_d_pop <- paste0(
+    i,
+    "_share_population"
+  )
+  
+  d_var_d_pop <- paste0(
+    "d_",
+    i,
+    "_share_population"
+  )
+  
+  # Contemporaneous employment / population ratio
+  reg[, (var_d_pop) :=
+        get(i) / population]
+  
+  # Change in employment / population ratio
+  reg[, (d_var_d_pop) :=
+        get(var_d_pop) -
+        shift(
+          get(var_d_pop),
+          type = "lag",
+          n = 1
+        ),
+      by = area_fips]
+  
+  w_d_var_d_pop <- paste0(
+    "w_",
+    d_var_d_pop
+  )
+  
+  q99 <- quantile(
+    reg[[d_var_d_pop]],
+    .99,
+    na.rm = TRUE
+  )
+  
+  q01 <- quantile(
+    reg[[d_var_d_pop]],
+    .01,
+    na.rm = TRUE
+  )
+  
+  reg[, (w_d_var_d_pop) :=
+        get(d_var_d_pop)]
+  
+  reg[
+    get(w_d_var_d_pop) >= q99,
+    (w_d_var_d_pop) := q99
+  ]
+  
+  reg[
+    get(w_d_var_d_pop) <= q01,
+    (w_d_var_d_pop) := q01
+  ]
+}
+
+reg[, workplace_emp_share_resident_emp := workplace_emp / resident_emp]
+reg[, d_workplace_emp_share_resident_emp := 
+      workplace_emp_share_resident_emp - shift(workplace_emp_share_resident_emp, type = "lag", n = 1), 
+    by = .(area_fips)]
+################################################################################
+# Save
+################################################################################
+
+fwrite(
+  reg,
+  paste0(
+    path,
+    "/output/transformed_reg.csv"
+  )
+)
