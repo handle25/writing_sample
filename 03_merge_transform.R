480444cc0a6c4fee7232b45c5da8a4391133816c
sohandle <handleysoph@gmail.com>
2026-08-23 15:12:25 -0400

    cleanup

--- 03_merge_transform.R
+++ 03_merge_transform.R
@@ -11,87 +11,50 @@ path <- "D:/writing_sample/data"
 local <- "C:/Users/Sophie/Desktop/phd_apps/writing_sample/data"
 setwd(path)
 
+# define functions -------------------------------------------------------------
+winsor <- function(dt, var, p = 0.01) {
+  
+  q <- quantile(
+    dt[[var]],
+    probs = c(p, 1 - p),
+    na.rm = TRUE
+  )
+  
+  w_var <- paste0("w_", var)
+  
+  dt[, (w_var) := pmin(
+    pmax(get(var), q[1]),
+    q[2]
+  )]
+}
+
+make_share_diff <- function(dt, num, denom, id = "area_fips") {
+  
+  share_var <- paste0(num, "_share_", denom)
+  diff_var  <- paste0("d_", share_var)
+  
+  dt[, (share_var) := get(num) / get(denom)]
+  
+  dt[, (diff_var) :=
+       get(share_var) -
+       shift(get(share_var), 1),
+     by = id]
+  
+  winsor(dt, diff_var)
+}
+
+diff_denom_all <- function(dt, var) { 
+    make_share_diff(dt, var, "resident_emp")
+    make_share_diff(dt, var, "workplace_emp")
+    make_share_diff(dt, var, "outside_jobs")
+    make_share_diff(dt, var, "total_servc_jobs")
+    make_share_diff(dt, var, "total_goods_jobs")
+    make_share_diff(dt, var, "population")
+  }
 ################################################################################
 # Population
 ################################################################################
-
-acs <- rbind(
-  fread(paste0(path, "/acs/co-est00int-tot.csv")), 
-  fread(paste0(path, "/acs/co-est2020.csv")),
-  fill = TRUE
-)
-
-acs <- melt(
-  acs,
-  id.vars = c("STATE", "COUNTY"),
-  measure.vars = patterns("^POPESTIMATE"),
-  variable.name = "year",
-  value.name = "population"
-) |>
-  fmutate(year = as.integer(substr(year, 12, 15)))
-
-# Drop rows created from years not covered by that source file
-acs <- acs[!is.na(population)]
-
-# Drop state totals
-acs <- acs[COUNTY != 0]
-
-# Create county FIPS
-acs[, area_fips := as.integer(paste0(
-  sprintf("%02d", STATE),
-  sprintf("%03d", COUNTY)
-))]
-
-
-# 1995 population --------------------------------------------------------------
-
-url_1995 <- paste0(
-  "https://www2.census.gov/programs-surveys/popest/",
-  "tables/1990-2000/intercensal/st-co/stch-icen1995.txt"
-)
-
-pop1995 <- fread(url_1995)
-
-names_1995 <- c(
-  "year",
-  "area_fips",
-  "age",
-  "sex",
-  "eth",
-  "population"
-)
-
-names(pop1995) <- names_1995
-
-pop1995 <- pop1995 |> 
-  fgroup_by(area_fips) |> 
-  fsummarize(
-    population = fsum(population)
-  ) |> 
-  fmutate(
-    year = 1995,
-    area_fips = as.integer(area_fips)
-  ) |>
-  data.table()
-
-# Keep only variables needed for merge
-acs <- acs[, .(
-  area_fips,
-  year,
-  population
-)]
-
-pop1995 <- pop1995[, .(
-  area_fips,
-  year,
-  population
-)]
-
-acs <- rbind(
-  acs,
-  pop1995,
-  fill = TRUE
-)
+acs <- fread(paste0(path, "/acs/population_1995_2023.csv"))
 
 
 ################################################################################
@@ -145,6 +108,7 @@ irs[year %in% c(2007:2012), new_year := 2013]
 irs[, year := new_year]
 irs <- irs |> fgroup_by(year, area_fips) |> 
   fsum()
+
 
 ################################################################################
 # Merge datasets
@@ -218,95 +182,11 @@ reg[, statefip :=
 # Net migration flows
 ################################################################################
 
-reg[, net_migration :=
-      as.integer(returns_3_inflow) -
-      as.integer(returns_3_outflow)]
+reg[, net_migration := (returns_3_inflow) - (returns_3_outflow)]
 
 # Net migration relative to employed residents
-reg[, net_migration_share_resident_emp :=
-      net_migration / resident_emp]
-
-reg[, net_migration_share_workplace_emp :=
-      net_migration / workplace_emp]
-
-reg[, net_migration_share_population :=
-      net_migration / population]
-
-reg[, net_migration_share_population_t0 :=
-      net_migration / shift(population, type = "lag", n = 1), by = .(area_fips)]
-
-reg[, d_net_migration_pctchange :=
-      (net_migration -
-         shift(net_migration, n = 1)) /
-      shift(net_migration, n = 1),
-    by = area_fips]
-
-
-# Winsorize net migration share -----------------------------------------------
-for (v in c("net_migration_share_workplace_emp", 
-            "net_migration_share_resident_emp", 
-            "net_migration_share_population",
-            "net_migration_share_population_t0")){
-  w_var <- paste0("w_", v)
-  
-  q99 <- quantile(
-    reg[, get(v)],
-    .99,
-    na.rm = TRUE
-  )
-  
-  q01 <- quantile(
-    reg[, get(v)],
-    .01,
-    na.rm = TRUE
-  )
-  
-  reg[, (w_var) :=
-        get(v)]
-  
-  reg[
-    get(v) >= q99,
-    (w_var) := q99
-  ]
-  
-  reg[
-    get(v) <= q01,
-    (w_var) := q01
-  ]
-  
-  
-  
-}
-
-
-
-# Winsorize change in net migration -------------------------------------------
-
-q99 <- quantile(
-  reg[, d_net_migration_pctchange],
-  .99,
-  na.rm = TRUE
-)
-
-q01 <- quantile(
-  reg[, d_net_migration_pctchange],
-  .01,
-  na.rm = TRUE
-)
-
-reg[, w_d_net_migration_pctchange :=
-      d_net_migration_pctchange]
-
-reg[
-  d_net_migration_pctchange >= q99,
-  w_d_net_migration_pctchange := q99
-]
-
-reg[
-  d_net_migration_pctchange <= q01,
-  w_d_net_migration_pctchange := q01
-]
-
+diff_denom_all(reg, "net_migration")
+diff_denom_all(reg, "resident_emp")
 
 ################################################################################
 # Employment-to-population ratios
@@ -321,81 +201,6 @@ reg[, workplace_emp_population_ratio :=
       workplace_emp / population]
 
 
-# Differences ------------------------------------------------------------------
-
-reg[, d_resident_emp_population_ratio :=
-      resident_emp_population_ratio -
-      shift(
-        resident_emp_population_ratio,
-        n = 1
-      ),
-    by = area_fips]
-
-reg[, d_workplace_emp_population_ratio :=
-      workplace_emp_population_ratio -
-      shift(
-        workplace_emp_population_ratio,
-        n = 1
-      ),
-    by = area_fips]
-
-
-# Winsorize resident employment / population ----------------------------------
-
-q99 <- quantile(
-  reg$d_resident_emp_population_ratio,
-  .99,
-  na.rm = TRUE
-)
-
-q01 <- quantile(
-  reg$d_resident_emp_population_ratio,
-  .01,
-  na.rm = TRUE
-)
-
-reg[, w_d_resident_emp_population_ratio :=
-      d_resident_emp_population_ratio]
-
-reg[
-  d_resident_emp_population_ratio >= q99,
-  w_d_resident_emp_population_ratio := q99
-]
-
-reg[
-  d_resident_emp_population_ratio <= q01,
-  w_d_resident_emp_population_ratio := q01
-]
-
-
-# Winsorize workplace employment / population ---------------------------------
-
-q99 <- quantile(
-  reg$d_workplace_emp_population_ratio,
-  .99,
-  na.rm = TRUE
-)
-
-q01 <- quantile(
-  reg$d_workplace_emp_population_ratio,
-  .01,
-  na.rm = TRUE
-)
-
-reg[, w_d_workplace_emp_population_ratio :=
-      d_workplace_emp_population_ratio]
-
-reg[
-  d_workplace_emp_population_ratio >= q99,
-  w_d_workplace_emp_population_ratio := q99
-]
-
-reg[
-  d_workplace_emp_population_ratio <= q01,
-  w_d_workplace_emp_population_ratio := q01
-]
-
-
 ################################################################################
 # Variables to difference in levels
 ################################################################################
@@ -408,356 +213,8 @@ level_vars <- c(
 
 level_vars <- unique(level_vars)
 
-
 for (i in level_vars) {
-  
-  ##############################################################################
-  # Denominator = resident employment
-  ##############################################################################
-  
-  var_d_all <- paste0(
-    i,
-    "_share_resident_emp"
-  )
-  
-  d_var_d_all <- paste0(
-    "d_",
-    i,
-    "_share_resident_emp"
-  )
-  
-  reg[, (var_d_all) :=
-        get(i) / resident_emp]
-  
-  reg[, (d_var_d_all) :=
-        get(var_d_all) -
-        shift(
-          get(var_d_all),
-          type = "lag",
-          n = 1
-        ),
-      by = area_fips]
-  
-  w_var_d_all <- paste0(
-    "w_",
-    d_var_d_all
-  )
-  
-  q99 <- quantile(
-    reg[[d_var_d_all]],
-    .99,
-    na.rm = TRUE
-  )
-  
-  q01 <- quantile(
-    reg[[d_var_d_all]],
-    .01,
-    na.rm = TRUE
-  )
-  
-  reg[, (w_var_d_all) :=
-        get(d_var_d_all)]
-  
-  reg[
-    get(w_var_d_all) >= q99,
-    (w_var_d_all) := q99
-  ]
-  
-  reg[
-    get(w_var_d_all) <= q01,
-    (w_var_d_all) := q01
-  ]
-  
-  ##############################################################################
-  # Denominator = local workplace employment
-  ##############################################################################
-  
-  var_d_all <- paste0(
-    i,
-    "_share_workplace_emp"
-  )
-  
-  d_var_d_all <- paste0(
-    "d_",
-    i,
-    "_share_workplace_emp"
-  )
-  
-  reg[, (var_d_all) :=
-        get(i) / workplace_emp]
-  
-  reg[, (d_var_d_all) :=
-        get(var_d_all) -
-        shift(
-          get(var_d_all),
-          type = "lag",
-          n = 1
-        ),
-      by = area_fips]
-  
-  w_var_d_all <- paste0(
-    "w_",
-    d_var_d_all
-  )
-  
-  q99 <- quantile(
-    reg[[d_var_d_all]],
-    .99,
-    na.rm = TRUE
-  )
-  
-  q01 <- quantile(
-    reg[[d_var_d_all]],
-    .01,
-    na.rm = TRUE
-  )
-  
-  reg[, (w_var_d_all) :=
-        get(d_var_d_all)]
-  
-  reg[
-    get(w_var_d_all) >= q99,
-    (w_var_d_all) := q99
-  ]
-  
-  reg[
-    get(w_var_d_all) <= q01,
-    (w_var_d_all) := q01
-  ]
-  
-  
-  ##############################################################################
-  # Denominator = outside employment
-  ##############################################################################
-  
-  var_d_out <- paste0(
-    i,
-    "_share_outside_jobs"
-  )
-  
-  d_var_d_out <- paste0(
-    "d_",
-    i,
-    "_share_outside_jobs"
-  )
-  
-  reg[, (var_d_out) :=
-        get(i) / outside_jobs]
-  
-  reg[, (d_var_d_out) :=
-        get(var_d_out) -
-        shift(
-          get(var_d_out),
-          type = "lag",
-          n = 1
-        ),
-      by = area_fips]
-  
-  w_var_d_out <- paste0(
-    "w_",
-    d_var_d_out
-  )
-  
-  q99 <- quantile(
-    reg[[d_var_d_out]],
-    .99,
-    na.rm = TRUE
-  )
-  
-  q01 <- quantile(
-    reg[[d_var_d_out]],
-    .01,
-    na.rm = TRUE
-  )
-  
-  reg[, (w_var_d_out) :=
-        get(d_var_d_out)]
-  
-  reg[
-    get(w_var_d_out) >= q99,
-    (w_var_d_out) := q99
-  ]
-  
-  reg[
-    get(w_var_d_out) <= q01,
-    (w_var_d_out) := q01
-  ]
-  
-  
-  ##############################################################################
-  # Denominator = resident service employment
-  ##############################################################################
-  
-  var_d_svc <- paste0(
-    i,
-    "_share_service_jobs"
-  )
-  
-  d_var_d_svc <- paste0(
-    "d_",
-    i,
-    "_share_service_jobs"
-  )
-  
-  reg[, (var_d_svc) :=
-        get(i) / total_servc_jobs]
-  
-  reg[, (d_var_d_svc) :=
-        get(var_d_svc) -
-        shift(
-          get(var_d_svc),
-          type = "lag",
-          n = 1
-        ),
-      by = area_fips]
-  
-  w_var_d_svc <- paste0(
-    "w_",
-    d_var_d_svc
-  )
-  
-  q99 <- quantile(
-    reg[[d_var_d_svc]],
-    .99,
-    na.rm = TRUE
-  )
-  
-  q01 <- quantile(
-    reg[[d_var_d_svc]],
-    .01,
-    na.rm = TRUE
-  )
-  
-  reg[, (w_var_d_svc) :=
-        get(d_var_d_svc)]
-  
-  reg[
-    get(w_var_d_svc) >= q99,
-    (w_var_d_svc) := q99
-  ]
-  
-  reg[
-    get(w_var_d_svc) <= q01,
-    (w_var_d_svc) := q01
-  ]
-  
-  
-  ##############################################################################
-  # Denominator = resident goods employment
-  ##############################################################################
-  
-  var_d_good <- paste0(
-    i,
-    "_share_goods_jobs"
-  )
-  
-  d_var_d_good <- paste0(
-    "d_",
-    i,
-    "_share_goods_jobs"
-  )
-  
-  reg[, (var_d_good) :=
-        get(i) / total_goods_jobs]
-  
-  reg[, (d_var_d_good) :=
-        get(var_d_good) -
-        shift(
-          get(var_d_good),
-          type = "lag",
-          n = 1
-        ),
-      by = area_fips]
-  
-  w_var_d_good <- paste0(
-    "w_",
-    d_var_d_good
-  )
-  
-  q99 <- quantile(
-    reg[[d_var_d_good]],
-    .99,
-    na.rm = TRUE
-  )
-  
-  q01 <- quantile(
-    reg[[d_var_d_good]],
-    .01,
-    na.rm = TRUE
-  )
-  
-  reg[, (w_var_d_good) :=
-        get(d_var_d_good)]
-  
-  reg[
-    get(w_var_d_good) >= q99,
-    (w_var_d_good) := q99
-  ]
-  
-  reg[
-    get(w_var_d_good) <= q01,
-    (w_var_d_good) := q01
-  ]
-  
-  
-  ##############################################################################
-  # Denominator = population
-  ##############################################################################
-  
-  var_d_pop <- paste0(
-    i,
-    "_share_population"
-  )
-  
-  d_var_d_pop <- paste0(
-    "d_",
-    i,
-    "_share_population"
-  )
-  
-  # Contemporaneous employment / population ratio
-  reg[, (var_d_pop) :=
-        get(i) / population]
-  
-  # Change in employment / population ratio
-  reg[, (d_var_d_pop) :=
-        get(var_d_pop) -
-        shift(
-          get(var_d_pop),
-          type = "lag",
-          n = 1
-        ),
-      by = area_fips]
-  
-  w_d_var_d_pop <- paste0(
-    "w_",
-    d_var_d_pop
-  )
-  
-  q99 <- quantile(
-    reg[[d_var_d_pop]],
-    .99,
-    na.rm = TRUE
-  )
-  
-  q01 <- quantile(
-    reg[[d_var_d_pop]],
-    .01,
-    na.rm = TRUE
-  )
-  
-  reg[, (w_d_var_d_pop) :=
-        get(d_var_d_pop)]
-  
-  reg[
-    get(w_d_var_d_pop) >= q99,
-    (w_d_var_d_pop) := q99
-  ]
-  
-  reg[
-    get(w_d_var_d_pop) <= q01,
-    (w_d_var_d_pop) := q01
-  ]
+  diff_denom_all(reg, i)
 }
 
 reg[, workplace_emp_share_resident_emp := workplace_emp / resident_emp]
